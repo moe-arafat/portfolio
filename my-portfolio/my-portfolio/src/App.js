8822abd6ab5198fca67f5488d3a381f1d2d0b489
moe-arafat <moe_arafat@outlook.com>
2024-01-22 19:40:42 -0500

    initial portfolio structure

--- my-portfolio/my-portfolio/src/App.js
+++ my-portfolio/my-portfolio/src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import about from "./components/about";
+import contact from "./components/contact";
+import navbar from "./components/navbar";
+import projects from "./components/projects";
+import skills from "./components/skills";
 
 export default function App() {
-  return "App";
+  return (
+    <main>
+      <navbar />
+      <about />
+      <projects />
+      <skills />
+      <contact />
+    </main>
+  );
 }
