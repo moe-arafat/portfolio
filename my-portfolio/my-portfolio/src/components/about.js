98d0532bf34f0bf634f294cc510df40d68003581
moe-arafat <moe_arafat@outlook.com>
2024-01-24 00:07:59 -0500

    about page work in progress

--- my-portfolio/my-portfolio/src/components/about.js
+++ my-portfolio/my-portfolio/src/components/about.js
@@ -1,1 +1,18 @@
-export default function about(){}
+import React from "react";
+
+export default function about(){
+
+    return (
+        <section id="about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                        Hi, I'm Moe
+                        <br className="hidden lg:inline-block" />I love to build amazing applications.
+                    </h1>
+                </div>
+            </div>
+        </section>
+    )
+
+}
